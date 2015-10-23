d63f41a969bf330f1e24bde8c43005fd877b7df1
isuvorov <me@isuvorov.ru>
2015-10-23 20:56:43 +0400

    readFile

--- prj-2/files-plus.js
+++ prj-2/files-plus.js
@@ -2,3 +2,16 @@
 
 //fs
 //readFileSync
+
+//__dirname
+//__filename
+
+
+var file = require("fs").readFileSync(__dirname + "/files/a.txt");
+console.log(file);
+
+require("./fs2").readFile(__dirname + "/files/a.txt", function(err, data){
+    console.log(data);
+});
+
+
--- prj-2/fs2.js
+++ prj-2/fs2.js
@@ -1,5 +1,10 @@
-module.exports = function(filename, next){
-    setTimeout(function(){
-        require("fs").readFile(filename, next);
-    },3000);
+//https://github.com/isuvorov/skillclub/blob/master/prj-2/fs2.js
+
+module.exports = {
+    readFile: function (filename, next) {
+        setTimeout(function () {
+            require("fs").readFile(filename, next);
+        }, 3000);
+    }
 };
+
