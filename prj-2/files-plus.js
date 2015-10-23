f054e692f9f663cd5358f733cb39fd80f935c145
isuvorov <me@isuvorov.ru>
2015-10-23 21:27:00 +0400

    async

--- prj-2/files-plus.js
+++ prj-2/files-plus.js
@@ -10,8 +10,93 @@
 var file = require("fs").readFileSync(__dirname + "/files/a.txt");
 console.log(file);
 
-require("./fs2").readFile(__dirname + "/files/a.txt", function(err, data){
-    console.log(data);
+//require("./fs2").readFile(__dirname + "/files/a.txt", function(err, fileA){
+//    require("./fs2").readFile(__dirname + "/files/b.txt", function(err, fileB){
+//        require("./fs2").readFile(__dirname + "/files/c.txt", function(err, fileC){
+//            console.log(fileA + fileB + fileC);
+//        });
+//    });
+//});
+
+var files = [
+    __dirname + "/files/a.txt",
+    __dirname + "/files/b.txt",
+    __dirname + "/files/c.txt",
+];
+
+var sum = 0;
+var i = 3;
+
+var async = require("async");
+
+
+async.map(files, require("./fs2").readFile, function(err, results){
+
+    var sum = 0;
+    for(i in results){
+        sum += parseInt(results[i]);
+    }
+
+    console.log({sum:sum});
+    // results is now an array of stats for each file
 });
+//
+//
+//require("./fs2").readFile(files[0], function (err, data) {
+//    sum += parseInt(data);
+//    i--;
+//    if(i == 0){
+//
+//        console.log({sum: sum});
+//    }
+//});
+//require("./fs2").readFile(files[1], function (err, data) {
+//    sum += parseInt(data);
+//    i--;
+//    if(i == 0){
+//
+//        console.log({sum: sum});
+//    }
+//});
+//require("./fs2").readFile(files[2], function (err, data) {
+//    sum += parseInt(data);
+//    i--;
+//    if(i == 0){
+//
+//        console.log({sum: sum});
+//    }
+//
+//});
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
