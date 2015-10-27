9de2014cd03bfef1d2a4ce9665a03b12b29b3da7
isuvorov <me@isuvorov.ru>
2015-10-27 15:54:30 +0400

    prj-5

---
+++ prj-5/README.md
@@ -0,0 +1,3 @@
+# prj-x
+
+a [Sails](http://sailsjs.org) application
---
+++ prj-5/api/policies/isReqName.js
@@ -0,0 +1,19 @@
+/**
+ * sessionAuth
+ *
+ * @module      :: Policy
+ * @description :: Simple policy to allow any authenticated user
+ *                 Assumes that your login action in one of your controllers sets `req.session.authenticated = true;`
+ * @docs        :: http://sailsjs.org/#!/documentation/concepts/Policies
+ *
+ */
+module.exports = function(req, res, next) {
+
+	if(req.param("name") == "Igor"){
+		return res.forbidden('You are not permitted to perform this action.');
+	}
+	else{
+		 return next();
+	}
+
+};
