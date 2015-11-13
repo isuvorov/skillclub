36ae9fc1fb820914cd3429d237a1cb9ca399cb09
isuvorov <me@isuvorov.ru>
2015-11-13 20:58:31 +0400

    MMO Cookie Clicker Begin2

--- prj-clicker/api/controllers/GameController.js
+++ prj-clicker/api/controllers/GameController.js
@@ -1,5 +1,10 @@
-var game = {
-    state: {
+var Game;
+
+Game = (function () {
+    function Game() {
+    }
+
+    Game.prototype.state = {
         users: {
             "user1": {
                 id: "user1",
@@ -16,9 +21,34 @@ var game = {
                 score: 0
             }
         }
-    },
-    steal: function(userIdFrom, userIdTo, gold){
-        if(gold == null) gold = 2;
+    };
+
+
+    Game.prototype.addUser = function (name) {
+
+
+        var userId = "user" + (Object.keys(this.state.users).length + 1);
+        console.log("this.state", this.state, "name", name, "userId", userId);
+
+        var user = {
+            id: userId,
+            name: name,
+            gold: 100,
+            clicks: 0,
+            score: 0
+        };
+
+        this.state.users[userId] = user;
+
+        sendToSockets("addUser", user);
+        sendToSockets("setState", this.state);
+
+        return user;
+    };
+    Game.prototype.steal = function (userIdFrom, userIdTo, gold) {
+        if (gold == null) gold = 2;
+
+
         // проверки
 
         // крадем золото
@@ -29,15 +59,48 @@ var game = {
         this.state.users[userIdTo].score += gold * 100;
         this.state.users[userIdTo].clicks += 1;
 
-    }
+
+        sendToSockets("steal", {userIdFrom: userIdFrom, userIdTo: userIdTo, gold: gold});
+        sendToSockets("setState", this.state);
+    };
+
+    return Game;
+
+})();
+
+
+var game = new Game();
+
+var sockets = [];
+
+var sendToSockets = function (event, data) {
+    sockets.forEach(function (socketId) {
+        try {
+            sails.sockets.emit(socketId, event, data);
+        } catch (err) {
+        }
+    });
 };
+
+
+
 
 
 var GameController;
 module.exports = GameController = {
     index: function (req, res) {
-        return res.render("game/index", {game: game})
+        var authUser = req.session.user;
 
+        return res.render("game/index", {game: game, authUser: authUser})
+
+    },
+    auth: function (req, res) {
+        var name = req.param("name");
+        var user = game.addUser(name);
+        req.session.user = user;
+
+
+        return res.redirect("/");
     },
 
     rand: function (req, res) {
@@ -54,4 +117,35 @@ module.exports = GameController = {
         return res.json(req.user);
 
     },
+    subscribe: function (req, res) {
+        if (!req.isSocket) {
+            return res.badRequest('Only a client socket can subscribe to Louies.  You, sir, appear to be something... _else_.');
+        }
+
+
+        return res.json(req.user);
+
+    },
+    steal: function (req, res) {
+        game.steal(req.param("userIdFrom"), req.param("userIdTo"));
+
+        return res.json(game.state);
+
+    },
+    test: function (req, res) {
+
+        return res.json({sockets: sockets});
+
+    },
+    test2: function (req, res) {
+        if (!req.isSocket) {
+           return res.json({isSocket:false});
+        }
+
+        if (sockets.indexOf(req.socket.id) === -1) {
+            sockets.push(req.socket.id);
+        }
+        return res.json({socketId:req.socket.id})
+    },
+
 };
--- prj-clicker/api/libs/Game.js
+++ prj-clicker/api/libs/Game.js
@@ -1,14 +1,70 @@
 var Game;
 
-Game = (function() {
-  function Game() {}
+Game = (function () {
+    function Game() {
+    }
 
-  Game.prototype.state = {};
+    Game.prototype.state = {
+        users: {
+            "user1": {
+                id: "user1",
+                name: "Daniel Craig",
+                gold: 100,
+                clicks: 0,
+                score: 0
+            },
+            "user2": {
+                id: "user2",
+                name: "Brad Pitt",
+                gold: 100,
+                clicks: 0,
+                score: 0
+            }
+        }
+    };
 
-  Game.prototype.qwe = function() {
-    return this.state = 123;
-  };
 
-  return Game;
+    Game.prototype.addUser = function (name) {
+        var userId = "user" + Object.keys(this.state).length + 1;
+
+        var user = {
+            id: userId,
+            name: name,
+            gold: 100,
+            clicks: 0,
+            score: 0
+        };
+
+        this.state.users[userId] = user;
+
+        sendToSockets("addUser", user);
+        sendToSockets("setState", this.state);
+
+        return user;
+    };
+    Game.prototype.steal = function (userIdFrom, userIdTo, gold) {
+        if (gold == null) gold = 2;
+
+
+        // проверки
+
+        // крадем золото
+        this.state.users[userIdFrom].gold -= gold;
+        this.state.users[userIdTo].gold += gold;
+
+        // начисляем очки
+        this.state.users[userIdTo].score += gold * 100;
+        this.state.users[userIdTo].clicks += 1;
+
+
+        sendToSockets("steal", {userIdFrom: userIdFrom, userIdTo: userIdTo, gold: gold});
+        sendToSockets("setState", this.state);
+    };
+
+    return Game;
 
 })();
+
+
+module.exports = Game;
+
